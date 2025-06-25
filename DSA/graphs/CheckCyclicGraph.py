d0ee2c2a51a4f3fa77195c0244c8af2a1673dfd3
pavan <124360829+BhimaPavanTeja@users.noreply.github.com>
2025-06-25 21:19:16 +0530

    Add cycle detection in undirected graphs
    Introduced a Solution class with DFS-based cycle detection for undirected graphs in CheckCyclicGraph.py. Added example usage and edge list conversion.

---
+++ DSA/graphs/CheckCyclicGraph.py
@@ -0,0 +1,36 @@
+class Solution:
+    def dfs(self, i, vis, edges, prev):
+        vis[i] = True
+        
+        for x in edges[i]:
+            if vis[x] is False:
+                if self.dfs(x, vis, edges, i):
+                    return True
+            else:
+                if x != prev:
+                    return True
+                    
+        return False
+                    
+    def isCycle(self, V, edges):
+        ans = False
+        vis = [False]*V
+        
+        for i in range(V):
+            if vis[i] is False:
+                ans = ans or self.dfs(i,vis,edges,-1)
+        
+        return ans
+    
+edges_list = [[0, 1], [0, 2], [1, 2], [2, 3]]
+V = 4
+
+# Convert edge list to adjacency list
+edges = [[] for _ in range(V)]
+for u, v in edges_list:
+    edges[u].append(v)
+    edges[v].append(u)
+
+sol = Solution()
+print("It is a Cyclic Graph" if sol.isCycle(V, edges) else "No Cycle")
+
